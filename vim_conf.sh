f74d73a4ddfccb904a9475c862a83f973d016aa2
Daniel <dansmerino@gmail.com>
2020-10-07 13:50:26 +0200

    Fix if clause

--- vim_conf.sh
+++ vim_conf.sh
@@ -1,13 +1,16 @@
 
 if ! hash curl 2>/dev/null; then
     apt-get install curl -y
+fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if !hash pip 2>/dev/null; then
     apt-get install pip -y
+fi
 
 pip install jedi
 
-vim -c ":so .vimrc" -c":q"
+#vim -c ":so .vimrc" -c":q"
+
